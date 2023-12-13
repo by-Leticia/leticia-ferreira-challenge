f781a38828615a4f84807560e4390630d6f71b61
Letícia Ferreira <leticia.oli.fer02@gmail.com>
2023-12-13 18:13:43 -0300

    Feat: initial config

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import './style.scss';
+
+const meuNome = 'Letícia';
+
+console.log(meuNome);
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const EsLintPlugin = require('eslint-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/index.html',
+    }),
+    new EsLintPlugin(),
+    new MiniCssExtractPlugin({ filename: '[name].css' }),
+  ],
+  optimization: {
+    minimizer: [new CssMinimizerPlugin()],
+  },
+};
